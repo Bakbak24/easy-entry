aa85922f4d3bf210f703588f8478bc8c020c56c6
Bakbak24 <zakabak3@gmail.com>
2024-06-20 01:29:50 +0200

    chore: Refactor switching-steps.js for improved code readability and maintainability

--- js/switching-steps.js
+++ js/switching-steps.js
@@ -8,19 +8,13 @@ document.addEventListener("DOMContentLoaded", function() {
         const leftColumn = document.querySelector('.steps-left');
         const rightColumn = document.querySelector('.steps-right');
         if (leftColumn.children.length > 0) {
-          // Move the step from the left column back to the right column
           rightColumn.appendChild(leftColumn.children[0]);
-          // Add class "step-tw" to the moved step
           rightColumn.children[rightColumn.children.length - 1].classList.add('step-tw');
         }
-        // Move the clicked step to the left column
         leftColumn.appendChild(step);
-        // Remove class "step-tw" from the clicked step
         step.classList.remove('step-tw');
       } else {
-        // Move the clicked step after the first child in the right column
         stepsParent.insertBefore(step, stepsParent.childNodes[1]);
-        // Add class "step-th" to the clicked step
         step.classList.add('step-th');
       }
     });
